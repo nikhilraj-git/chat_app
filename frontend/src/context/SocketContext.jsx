791df21780756e39bf27ed1c19f461f399b64440
Nikhil__Raj <128668076+nikhilraj-git@users.noreply.github.com>
2024-07-01 01:02:56 +0530

    lh - deploy

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:3200/", {
+      const socket = io("https://chat-app-nyju.onrender.com", {
         query: {
           userId: authUser._id,
         },
